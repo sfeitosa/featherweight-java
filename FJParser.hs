2a7aa106d09969b5f2293cf2ef66baa8f24bbcb3
sfeitosa <35307953+sfeitosa@users.noreply.github.com>
2018-01-10 16:19:27 -0200

    First commit.

---
+++ FJParser.hs
@@ -0,0 +1,44 @@
+-- Haskell parser for Featherweight Java
+-- Author: Samuel da Silva Feitosa
+-- Date: 01/2018
+---------------------------------------------
+module FJParser where
+import Data.Map
+
+-- Syntax
+data Class = Class String String [(Type,String)] Constr [Method]
+           deriving (Show, Eq)
+
+data Constr = Constr String [(Type,String)] [String] [(String,String)]
+            deriving (Show, Eq)
+
+data Method = Method Type String [(Type,String)] Expr
+            deriving (Show, Eq)
+
+data Expr = Var String                               -- Variable
+          | FieldAccess Expr String                  -- Field Access
+          | MethodInvk Expr String [Expr]            -- Method Invocation
+          | CreateObject String [Expr]               -- Object Instantiation
+          | Cast String Expr                         -- Cast
+          deriving (Show, Eq)
+
+data Type = TypeClass String
+          deriving (Show, Eq)
+
+type Env = Map String Type
+type CT = Map String Class
+
+
+constr1 = Constr "A" [] [] []
+constr2 = Constr "B" [] [] []
+constr3 = Constr "C" [] [] []
+
+class1 = Class "A" "B" [(TypeClass "Object", "x"), 
+                             (TypeClass "Object", "y")] constr1 []
+class2 = Class "B" "C" [(TypeClass "Object", "z")] constr2 [meth1]
+class3 = Class "C" "Object" [(TypeClass "Object", "w")] constr3 [meth2] 
+
+meth1 = Method (TypeClass "Object") "getA" [(TypeClass "Object", "a")] (Var "a")
+meth2 = Method (TypeClass "Object") "getB" [(TypeClass "Object", "b")] (Var "b")
+
+ct = Data.Map.fromList [("A", class1), ("B", class2), ("C", class3)]
